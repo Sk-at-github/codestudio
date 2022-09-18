239e8e068fc9bf8ce81cd45ea0fd126ae20c1263
Sk-at-github <98371965+Sk-at-github@users.noreply.github.com>
2022-09-18 22:16:11 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -4,27 +4,15 @@
 
 
 void selectionSort(vector<int>& arr, int n)
-2
 {   
-3
     // Write your code here.
-4
     int j =0;
-5
     for(int i = 0;i<(n-1);i++){
-6
         int MinIndex = i;
-7
         for( j = i+1;j<n;j++){
-8
             if(arr[j]<arr[MinIndex]){
-9
             MinIndex = j;}
-10
         }
-11
-        swap(arr[i],arr[MinIndex]);
-12
+       swap(arr[i],arr[MinIndex]);
     }
-13
 }
